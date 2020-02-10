ddc49247de3d0cbce61544441bf095e35639e5c2
Robert Campbell <Campbell.Rob@protonmail.com>
2020-02-10 09:52:38 -0800

    spring cleaning, removed deps, folder structure

--- src/App.js
+++ src/App.js
@@ -1,33 +1,19 @@
-import React from 'react';
-import AccessibilityModal from './components/AccessibilityModal';
-import Header from './components/Header';
-import Home from './components/LandingPage';
-import Signup from './components/users/SignUp';
-import UserDashboard from './components/users/UserDashboard';
-import AdminDashboard from './components/admin/AdminDashboard';
-import PrivateRoute from './utils/PrivateRoute';
-import Login from './components/auth/Login';
-import Logout from './components/Logout';
+import React from "react";
+import UserDashboard from "./components/users/UserDashboard";
+import AdminDashboard from "./components/admin/AdminDashboard";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import EditProfile from './components/ProfileForm';
+import EditProfile from "./components/ProfileForm";
 import "./App.css";
 
 function App() {
   return (
-    <>
-    <Header />
     <div className="App">
-      {/* <AccessibilityModal /> */}
-      <Route exact path="/Edit" component={EditProfile} />
-      <Route exact path="/" component={Home} />
-      <Route exact path="/Signup" component={Signup} />
-      <Route exact path="/SignIn" component={Login} />
-      <Route exact path="/logout" component={Logout} />
-      <PrivateRoute exact path="/dashboard/admin" component={AdminDashboard} />
-      {/* <PrivateRoute exact path="/dashboard" component={UserDashboard} /> */}
-      <Route exact path="/dashboard/:id" component={UserDashboard} />
+      <Switch>
+        <Route exact path="/Edit" component={EditProfile} />
+        <Route exact path="/dashboard/admin" component={AdminDashboard} />
+        <Route exact path="/dashboard/:id" component={UserDashboard} />
+      </Switch>
     </div>
-    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { createStore, applyMiddleware } from 'redux'
-import { Provider } from 'react-redux';
-import { reducer } from './ReduxStuff/reducers/reducer';
-import logger from 'redux-logger';
-import thunk from 'redux-thunk';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
+//apollo deps
+import { ApolloClient } from "apollo-client";
+// import { InMemoryCache, NormalizedCacheObject } from "apollo-cache-inmemory";
+// import { HttpLink } from "apollo-link-http";
+import { ApolloProvider } from "@apollo/react-hooks";
+//!apollo deps
 
-const store = createStore(reducer, applyMiddleware(thunk, logger))
+import App from "./App";
 
+import * as serviceWorker from "./serviceWorker";
+
+const client = new ApolloClient({
+  //   cache,
+  //   link
+});
 
 ReactDOM.render(
-
-    <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
-    </Provider>
-
-
-
-    , document.getElementById('root'));
+  <ApolloProvider client={client}>
+    <Router>
+      <App />
+    </Router>
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
